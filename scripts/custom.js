733c7c64a7c3db925b8d86e52852ba8d4ac75aae
Otávio Moreira <otavio.moreira@leadforce.com.br>
2024-03-18 17:34:39 -0300

    projeto teste

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,45 +1,45 @@
-$(document).ready(function () {
-    (function ($) {
-      $(function () {
-        var isoCountries = [
-          {
-            id: "BR",
-            text: "Português"
-          },
-          {
-            id: "PT",
-            text: "Português"
-          },
-          {
-            id: "US",
-            text: "Inglês"
-          },
-        ];
+$('.dropdown > .caption').on('click', function() {
+    $(this).parent().toggleClass('open');
+});
+
+// $('.price').attr('data-currency', 'RUB');
+
+$('.dropdown > .list > .item').on('click', function() {
+    $('.dropdown > .list > .item').removeClass('selected');
+    $(this).addClass('selected').parent().parent().removeClass('open').children('.caption').html($(this).html());
+
+    if ($(this).data("item") == "RUB") {
+        console.log('RUB');
+    } else if ($(this).data("item") == "UAH") {
+        console.log('UAH');
+    } else {
+        console.log('USD');
+    }
+//         if ($(this).data("item") == "RUB") {
+//             $('.price').attr('data-currency', 'RUB');
+//             $('.currency').text('руб.');
+
+//         } else if ($(this).data("item") == "UAH") {
+//             $('.price').attr('data-currency', 'UAH');
+//             $('.currency').text('грн.');
+
+//         } else {
+//             $('.price').attr('data-currency', 'USD');
+//             $('.currency').text('долл.');
+//         }
   
-        function formatCountry(country) {
-          if (!country.id) {
-            return country.text;
-          }
-          var $country = $(
-            '<span class="flag-icon flag-icon-' +
-              country.id.toLowerCase() +
-              ' flag-icon-squared"></span>' +
-              '<span class="flag-text">' +
-              country.text +
-              "</span>"
-          );
-          return $country;
-        }
+
   
-        //Assuming you have a select element with name country
-        // e.g. <select name="name"></select>
-  
-        $("[name='country']").select2({
-          placeholder: "Selecione seu pais",
-          templateResult: formatCountry,
-          data: isoCountries
-        });
-      });
-    })(jQuery);
-  });
-  
+});
+
+$(document).on('keyup', function(evt) {
+    if ((evt.keyCode || evt.which) === 27) {
+        $('.dropdown').removeClass('open');
+    }
+});
+
+$(document).on('click', function(evt) {
+    if ($(evt.target).closest(".dropdown > .caption").length === 0) {
+        $('.dropdown').removeClass('open');
+    }
+});
